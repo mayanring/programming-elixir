9d3b8f9ed9455d47e79ae5cf9dd1f43dfe32b23e
Ryan Ming <ryan.ming@gmail.com>
2017-01-26 01:07:53 -0500

    realized first case already sets up a new midpoint each time it's called, removed extraneous code.

--- modules-and-functions-6.exs
+++ modules-and-functions-6.exs
@@ -1,22 +1,17 @@
 defmodule Chop do
-	def guess(actual, range = min..max) do
+	def guess(actual, min..max) do		
 		target = div(min + max, 2)
-		guess(actual, range, target)
-	end
-
-	def guess(actual, _range, actual) do
-		IO.puts "It's #{actual}!"
-	end
-
-	def guess(actual, min.._max, target) when target > actual do
 		IO.puts "Is it #{target}?"
-		new_target = div(min + target, 2)
-		guess(actual, min..target, new_target)
+		guess(actual, min..max, target)
+	end
+
+	def guess(actual, _range, actual), do: IO.puts "It's #{actual}!"
+
+	def guess(actual, min.._max, target) when target > actual do		
+		guess(actual, min..target)
 	end
 
 	def guess(actual, _min..max, target) when target < actual do
-		IO.puts "Is it #{target}?"
-		new_target = div(target + max, 2)
-		guess(actual, target..max, new_target)
+		guess(actual, target..max)
 	end
 end

253407464cb0549cb8d57647488925ac55481fda
Ryan Ming <ryan.ming@gmail.com>
2017-01-31 00:14:05 -0500

    add solutions for #each, #filter

--- lists-and-recursion-5.exs
+++ lists-and-recursion-5.exs
@@ -8,11 +8,26 @@
 
 
 defmodule MyList do
-	def all?([], func) do
-		true
-	end
+	def all?([], _func), do: true
 
 	def all?([ head | tail], func) do
 		func.(head) && all?(tail, func)
+	end
+
+	def each([], _func), do: :ok
+
+	def each([ head | tail], func) do
+		func.(head)
+		each(tail, func)
+	end
+
+	def filter([], _func), do: []
+
+	def filter([head | tail], func) do
+		if func.(head) do
+			[head | filter(tail, func)]
+		else
+			filter(tail, func)
+		end
 	end
 end

1640be92452739c0b91cb1b444100bfd9e4d1ebb
Ryan Ming <ryan.ming@gmail.com>
2017-02-07 13:42:50 -0500

    added solution for anagrams

---
+++ strings-and-binaries-2.exs
@@ -0,0 +1,7 @@
+# Write an anagram?(word1, word2) that returns true if its parameters are anagrams.
+
+defmodule MyString do
+  def anagram?(word1, word2) do
+    Enum.sort(word1) == Enum.sort(word2)
+  end
+end
